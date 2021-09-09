d01b49bb793f58564c1435780190fd3dfaec3ead
Chase Strackbein <chasestrackbein@gmail.com>
2021-09-08 22:06:33 -0400

    Add data generation

---
+++ generate.js
@@ -0,0 +1,14 @@
+const fs = require('fs-extra');
+
+const allData = [];
+
+const mods = fs.readdirSync('./mods', { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name);
+
+for (const mod of mods) {
+  const data = fs.readJSONSync(`./mods/${mod}/data.json`);
+  allData.push(data);
+}
+
+fs.writeJSONSync(`./mods/all.json`, allData);
