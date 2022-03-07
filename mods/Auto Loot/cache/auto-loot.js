b2707dba175f4854d51d5cdfcf669d847d4c3d76
Chase Strackbein <chasestrackbein@gmail.com>
2022-03-06 19:37:52 -0500

    Add Polfy mods

---
+++ mods/Auto Loot/cache/auto-loot.js
@@ -0,0 +1,110 @@
+// ==UserScript==
+// @name            Melvor Auto Loot
+// @version         1.5.3
+// @license         MIT
+// @description     Add an Auto Loot toggle in combat
+// @author          Polfy
+// @match           https://*.melvoridle.com/*
+// @exclude         https://wiki.melvoridle.com*
+// @namespace https://greasyfork.org/users/234234
+// ==/UserScript==
+
+function AutoLoot() {
+  ScriptLog();
+
+  // Grab Melvor data
+  const MELVOR = {
+      combatLootContainer: $("#combat-loot-container"),
+
+      // Loot btn section
+      lootAllBtn: document.querySelector("#combat-btn-loot-all"),
+
+      lootAll() {
+          combatManager.loot.lootAll(); // Loot all function
+      },
+
+      // // Melvor autoloot switch
+      // setAutoLooting(val) {
+      //     player.modifiers.autoLooting = val;
+      // },
+  };
+
+  var autoLootIsEnable = false;
+
+  MELVOR.combatLootContainer.bind("DOMNodeInserted", () => {
+      if (autoLootIsEnable) {
+          MELVOR.lootAll();
+      }
+  });
+
+  // Adding the button
+  const br = document.createElement("br");
+  MELVOR.lootAllBtn.parentNode.appendChild(br);
+
+  var autoLootBtn = document.createElement("button");
+  autoLootBtn.addEventListener("click", toggleAutoLoot);
+  autoLootBtn.classList.add(...["btn", "btn-sm", "mt-2"]);
+  autoLootBtn.classList.add("btn-danger");
+  autoLootBtn.innerText = "Auto Loot : Disabled";
+  MELVOR.lootAllBtn.parentNode.appendChild(autoLootBtn);
+
+  function toggleAutoLoot() {
+      if (!autoLootIsEnable) {
+          enableAutoLoot();
+      } else {
+          disableAutoLoot();
+      }
+      autoLootIsEnable = !autoLootIsEnable;
+  }
+
+  function enableAutoLoot() {
+      // MELVOR.setAutoLooting(1);
+
+      MELVOR.lootAll();
+
+      autoLootBtn.classList.remove("btn-danger");
+      autoLootBtn.classList.add("btn-warning");
+      autoLootBtn.innerText = "Auto Loot : Enabled";
+  }
+
+  function disableAutoLoot() {
+      // MELVOR.setAutoLooting(0);
+
+      autoLootBtn.classList.remove("btn-warning");
+      autoLootBtn.classList.add("btn-danger");
+      autoLootBtn.innerText = "Auto Loot : Disabled";
+  }
+
+  function ScriptLog() {
+      console.log("Melvor Auto Loot: Loaded [Contact info : https://discord.gg/hAdGcWc4nY | Polfy#6924]");
+
+      if (!document.querySelector("#PolfyUserScriptsDiscordLink")) {
+          let llink = document.createElement("li");
+          llink.id = "PolfyUserScriptsDiscordLink";
+          llink.classList.add("nav-main-item");
+          llink.innerHTML = `
+              <a class="nav-main-link nav-compact" href="https://discord.gg/hAdGcWc4nY" target="_blank">
+                  <img class="nav-img" src="https://cdn.melvor.net/core/v018/assets/media/main/discord.svg">
+                  <span class="nav-main-link-name page-nav-name-misc-15">Polfy UserScript Discord</span>
+              </a>
+          `;
+          document.querySelector(".nav-main-heading.page-nav-name-misc-12").after(llink);
+      }
+  }
+}
+
+// Injecting the script when possible
+(() => {
+  function loadScript() {
+      // Load script after the actual Melvor game has loaded
+      if (typeof isLoaded !== typeof undefined && isLoaded) {
+          clearInterval(scriptLoader);
+
+          const scriptElem = document.createElement("script");
+          scriptElem.textContent = `try {(${AutoLoot})();} catch (e) {console.log(e);}`;
+          document.body.appendChild(scriptElem).parentNode.removeChild(scriptElem);
+      }
+  }
+
+  const scriptLoader = setInterval(loadScript, 250);
+})();
---
+++ mods/One Click Farming/cache/one-click-farming.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name            Melvor One Click Farming
+// @version         1.1.1
+// @license         MIT
+// @description     Harvest All, Apply Weird Gloop to all Plots and Plant All Selected Crops on every farming areas in one click
+// @author          Polfy
+// @match           https://*.melvoridle.com/*
+// @exclude         https://wiki.melvoridle.com*
+// @namespace https://greasyfork.org/users/234234
+// ==/UserScript==
+
+function Farming() {
+    ScriptLog();
+
+    // Grab Melvor data
+    const MELVOR = {
+        tippy: tippy,
+
+        goldIcon: '<img class="skill-icon-xxs" src="https://cdn.melvor.net/core/v018/assets/media/main/coins.svg">',
+
+        farmingBtnsContainer: document.querySelector("#farming-container .col-12.col-md-4 .ml-2.mr-2.text-right"),
+
+        loadFarmingArea: loadFarmingArea,
+        harvestAll: harvestAll,
+        gloopAll: gloopAll,
+        plantAllSelectedCrops: plantAllSelectedCrops,
+    };
+
+    const btn = document.createElement("button");
+    btn.id = "WIP-FARMING";
+    btn.classList.add(...["btn", "btn-sm", "btn-warning", "m-1"]);
+    btn.innerText = "Harvest & Plant All";
+    btn.addEventListener("click", () => {
+        for (let i = 0; i < 3; i++) {
+            MELVOR.loadFarmingArea(i);
+            MELVOR.harvestAll();
+            MELVOR.gloopAll();
+            MELVOR.plantAllSelectedCrops();
+        }
+
+        MELVOR.loadFarmingArea(0);
+    });
+
+    MELVOR.farmingBtnsContainer.appendChild(btn);
+    MELVOR.tippy("#WIP-FARMING", {
+        content: `
+            Click to perform these actions on all farming plots:
+            <br>
+            -> Harvest All
+            <br>
+            -> Apply Weird Gloop to all Plots
+            <br>
+            -> Plant All Selected Crops
+            <br><br>
+            Max cost: ${MELVOR.goldIcon} 27000`,
+        allowHTML: true,
+    });
+
+    // Polfy Melvor UserScripts Log
+    function ScriptLog() {
+        console.log("Melvor One Click Farming: Loaded [Contact info : https://discord.gg/hAdGcWc4nY | Polfy#6924]");
+
+        if (!document.querySelector("#PolfyUserScriptsDiscordLink")) {
+            let llink = document.createElement("li");
+            llink.id = "PolfyUserScriptsDiscordLink";
+            llink.classList.add("nav-main-item");
+            llink.innerHTML = `
+                <a class="nav-main-link nav-compact" href="https://discord.gg/hAdGcWc4nY" target="_blank">
+                    <img class="nav-img" src="https://cdn.melvor.net/core/v018/assets/media/main/discord.svg">
+                    <span class="nav-main-link-name page-nav-name-misc-15">Polfy UserScript Discord</span>
+                </a>
+            `;
+            document.querySelector(".nav-main-heading.page-nav-name-misc-12").after(llink);
+        }
+    }
+}
+
+// Injecting the script when possible
+(() => {
+    function loadScript() {
+        // Load script after the actual Melvor game has loaded
+        if (typeof isLoaded !== typeof undefined && isLoaded) {
+            clearInterval(scriptLoader);
+
+            const scriptElem = document.createElement("script");
+            scriptElem.textContent = `try {(${Farming})();} catch (e) {console.log(e);}`;
+            document.body.appendChild(scriptElem).parentNode.removeChild(scriptElem);
+        }
+    }
+
+    const scriptLoader = setInterval(loadScript, 250);
+})();
